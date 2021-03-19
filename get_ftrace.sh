9e4945aac8e8e68620a54b09036dbb888979bd87
pwr2011 <37601696+pwr2011@users.noreply.github.com>
2021-03-19 19:56:26 +0900

    Create get_ftrace.sh

---
+++ get_ftrace.sh
@@ -0,0 +1,8 @@
+  GNU nano 3.2                     get_ftrace.sh                                
+
+#!/bin/bash
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+echo "ftrace off"
+sleep 3
+cp /sys/kernel/debug/tracing/trace .
+mv trace ftrace_log.c
